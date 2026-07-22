d9830e175c8d2e0b91ccf85ce3c99cee2913e06c
Mimanshu Gahlaut <mimanshugahlaut10@gmail.com>
2026-07-23 01:11:35 +0530

    Add palindrome linked list solution
    Implement palindrome check for singly-linked list.

---
+++ Session-04/PallindromeLinkedList.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if(!head || !head->next) return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* second = reverse(slow->next);
+        ListNode* first = head;
+        while (second) {
+            if (first->val != second->val) return false;
+            
+            first = first->next;
+            second = second->next;
+        }
+        return true;
+    }
+};
