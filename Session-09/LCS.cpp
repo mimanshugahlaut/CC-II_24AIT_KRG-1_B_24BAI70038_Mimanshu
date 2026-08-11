908e5828236f60c182096d0d12d4423574a67419
Mimanshu Gahlaut <mimanshugahlaut10@gmail.com>
2026-08-11 22:41:27 +0530

    Refactor LCS implementation to use 2D DP array

--- Session-09/LCS.cpp
+++ Session-09/LCS.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int a = text1.length();
-        int b = text2.length();
+        int a = text1.size();
+        int b = text2.size();
 
-        vector<int> prev(b + 1, 0);
-        vector<int> curr(b + 1, 0);
+        vector<vector<int>> dp(a + 1, vector<int> (b + 1, 0));
 
         for (int i = 1; i <= a; i++) {
             for (int j = 1; j <= b; j++) {
-                if (text1[i - 1] == text2[j - 1]) curr[j] = 1 + prev[j - 1];
-                else curr[j] = max(curr[j - 1], prev[j]);
+                if (text1[i - 1] == text2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
-        prev = curr;
         }
+        return dp[a][b];
 
-        return prev[b];
     }
 };
